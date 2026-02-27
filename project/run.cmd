a17dadab85a316500d36a204ecbd1fcd3d7a8d8d
togetherfrist <251220067@nju.edu.cn>
2026-02-27 21:42:00 +0800

    small adjustment

--- project/run.cmd
+++ project/run.cmd
@@ -3,4 +3,5 @@ cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE 
 cmake --build ./build --config Debug --target all --
 cd ../
 copy ".\project\build\skill2048.exe"
+del ".\project\build\skill2048.exe"
 skill2048.exe
