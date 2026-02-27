5e177c78f61452b3f6bd850266a368d9fde8d841
togetherfrist <251220067@nju.edu.cn>
2026-02-27 13:25:59 +0800

    rename

--- run.cmd
+++ run.cmd
@@ -1,4 +1,4 @@
 cmake -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE --no-warn-unused-cli -S . -B ./build
 cmake --build ./build --config Debug --target all --
-copy ".\build\my2048.exe"
-.\my2048.exe
+copy ".\build\skill2048.exe"
+.\skill2048.exe
